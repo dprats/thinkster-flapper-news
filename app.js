a6acd8df92a2276b9ece7deb615905cc3e13b69d
Diego Prats <diego.prats@gmail.com>
2015-08-16 00:20:38 -0700

    angular services

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-angular.module('flapperNews',[]);
+var app = angular.module('flapperNews',[]);
 
-angular.module('flapperNews').controller('MainCtrl',['$scope', function($scope){
+app.controller('MainCtrl',['$scope','posts', function($scope, posts){
 
 	$scope.test = 'hello world';
-	$scope.posts = [
-		{title: 'post 1', upvotes: 5 },
-	  	{title: 'post 2', upvotes: 2},
-		{title: 'post 3', upvotes: 15},
-		{title: 'post 4', upvotes: 9},
-		{title: 'post 5', upvotes: 4}
-	];
+	$scope.posts = posts.posts;
+	// $scope.posts = [
+	// 	{title: 'post 1', upvotes: 5 },
+	//   	{title: 'post 2', upvotes: 2},
+	// 	{title: 'post 3', upvotes: 15},
+	// 	{title: 'post 4', upvotes: 9},
+	// 	{title: 'post 5', upvotes: 4}
+	// ];
 
 	//Create a $scope function that will add an object into the posts array:
 	$scope.addPost = function(){
@@ -27,5 +28,16 @@ angular.module('flapperNews').controller('MainCtrl',['$scope', function($scope){
 	$scope.incrementUpvotes = function(post){
 		post.upvotes += 1;
 	}
+
+}]);
+
+//creating a factory to store the data
+app.factory('posts', [function(){
+
+	var o = {
+		posts: []
+	}
+
+	return o;
 
 }]);
