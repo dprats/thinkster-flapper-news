2c02faa1aff4cd761be3979188a88b9ac909cc20
Diego Prats <diego.prats@gmail.com>
2015-08-17 00:27:59 -0700

    create schemas with mongoose

---
+++ flapper-news/models/Comments.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+
+var CommentSchema = new Schema({
+
+	body: String,
+	author: String,
+	upvotes: { type: Number, default: 0},
+	post: {type: mongoose.Schema.Type.ObjectId, ref: 'Post'}
+});
+
+mongoose.model('Comment', CommentSchema);
