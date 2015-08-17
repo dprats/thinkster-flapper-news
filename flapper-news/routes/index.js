a5c347525b270ce5cc801ad8697a31247c00cc1f
Diego Prats <diego.prats@gmail.com>
2015-08-17 00:53:37 -0700

    Opening Rest routes/pre-loading objects

--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -7,3 +7,56 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+//start by creating a GET route for retrieving posts in routes/index.js. 
+//It should return a JSON list containing all posts:
+
+var mongoose = require('mongoose');
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
+
+router.get('/posts', function(req,res, next){
+
+	Post.find(function(err,posts){
+		if (err){ return next(err); }
+
+		res.json(posts);
+	})
+
+});
+
+
+//creating a POST route for creating posts:
+
+router.post('/posts', function(req,res,next){
+
+	var post = new Post(req.body);
+
+	post.save(function(err, post){
+		if(err) {return next(err); }
+
+		res.json(post);	
+	});
+
+});
+
+//Rather than replicating the same code for the remaining routes across several
+//different request handlers, we use the param() function on Express.js to loan an object
+
+router.param('post', function(req,res,next,id){
+
+	var query = Post.findById(id);
+
+	query.exec(function(err, post){
+		if (err) { return next(err); }
+		if (!post) { return next(new Error('can\'t find post')); }
+
+		req.post = post;
+		return next();
+	});
+});
+
+//create our route for returning a single post:
+router.get('/posts/:post', function(req,res){
+	res.json(req.post);
+});
